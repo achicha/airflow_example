66c8fea05fab85eb7fbf0d00f779a83e9d63bd21
achicha <meth787@gmail.com>
2022-07-05 18:56:56 +0300

    separate dag_run tests

--- tests/test_twitter_run_dag.py
+++ tests/test_twitter_run_dag.py
@@ -7,8 +7,21 @@ def dagbag():
     return DagBag()
 
 
-def test_dag_loaded(dagbag):
-    dag = dagbag.get_dag(dag_id="twitter_operations")
+def test_dag_import_errors(dagbag):
     assert dagbag.import_errors == {}
-    assert dag is not None
-    assert len(dag.tasks) == 3
+
+
+def test_dag_is_not_none(dagbag):
+    for dag_id in dagbag.dag_ids:
+        dag = dagbag.get_dag(dag_id=dag_id)
+        assert dag is not None
+
+
+@pytest.mark.parametrize('current_dag_id', ['twitter_operations'])
+@pytest.mark.parametrize('tasks_count', [3])
+def test_dag_tasks_count(dagbag, current_dag_id, tasks_count):
+    for dag_id in dagbag.dag_ids:
+        if dag_id == current_dag_id:
+            dag = dagbag.get_dag(dag_id=dag_id)
+            assert len(dag.tasks) == tasks_count
+
